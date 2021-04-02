bb59de93d60e6970ffd45ea17c1dba908e541719
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 15:05:30 +0200

    imported the styles and added buttom for projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+
+// must be checked:
+// https://www.gatsbyjs.com/docs/reference/release-notes/migrating-from-v2-to-v3/#css-modules-are-imported-as-es-modules
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Develop</h2>
           <h3>Design & Deploy</h3>
           <p>Web developer & designer based in Berlin</p>
+          <Link className={styles.btn} to="/projects">
+            My projects
+          </Link>
         </div>
       </section>
     </Layout>

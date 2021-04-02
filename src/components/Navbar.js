6e188710fa64da5c1c45dcd68358f4d0a207dd16
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 12:02:21 +0200

    added initial navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,17 @@
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Home() {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <h1>Mohammad Kharma</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/timeline">Timeline</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </nav>
+  )
 }

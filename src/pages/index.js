bad504d7a1b1ae34b4f38992f15c1720972d4393
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 12:01:57 +0200

    corrected typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Home() {
   return <div>
     <h1>Develop</h1>
-    <h2>Design & Deploy</h2>
+    <h3>Design & Deploy</h3>
     <p>Web developer & designer based in Berlin</p>
   </div>
 }

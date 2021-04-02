b1d1fd71894f0532a1a4fc66fe7ac4078cddc751
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 20:38:58 +0200

    added paragraph to each page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -8,6 +8,15 @@ export default function Projects() {
       <div className={styles.projects}>
         <h2>Projects</h2>
         <h3>& websites I've created</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
     </Layout>
   )

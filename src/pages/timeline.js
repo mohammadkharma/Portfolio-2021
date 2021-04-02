0eb24e977abd46b5dc5e75c95d1d1cd253adb5f9
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 12:33:17 +0200

    added a timeline page

---
+++ src/pages/timeline.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function Timeline() {
+  return (
+    <Layout>
+      <h1>Timeline</h1>
+    </Layout>
+  )
+}

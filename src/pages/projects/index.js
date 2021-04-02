62e25e6e3b4ba069ad85460a5817f82e841e52db
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 15:45:01 +0200

    imported the css module and applied the styles

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <h2>Projects</h2>
-      <h3>$ website I've created</h3>
+      <div className={styles.projects}>
+        <h2>Projects</h2>
+        <h3>& websites I've created</h3>
+      </div>
     </Layout>
   )
 }

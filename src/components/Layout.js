ae07f667b522d7729e9b764ececba524cf46e849
MYK <mohammad.kh.8688@gmail.com>
2021-04-02 12:31:48 +0200

    nested the children inte the content div

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Navbar from "../components/Navbar"
+import Navbar from "./Navbar"
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <div className="layout">
       <Navbar />
-      <div className="content"></div>
+      <div className="content">{children}</div>
       <footer>
         <p>Copyright 2021 Mohammad Kharma</p>
       </footer>

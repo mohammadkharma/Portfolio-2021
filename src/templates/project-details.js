f9b10c3ee3ca32c9124d1dc54f1ecb05cac18580
MYK <mohammad.kh.8688@gmail.com>
2021-04-03 21:39:56 +0200

    added a project-details template with initial content

---
+++ src/templates/project-details.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/Layout"
+import * as styles from "../styles/projects.module.css"
+
+export default function ProjectDetails() {
+  return (
+    <div>
+      <Layout>
+        <div className={styles.project - details}>
+          <h2>title</h2>
+          <h3>stack</h3>
+          <div>
+            <Img fluid={} />
+          </div>
+          <div className={styles.html} dangerouslySetInnerHTML={} />
+        </div>
+      </Layout>
+    </div>
+  )
+}

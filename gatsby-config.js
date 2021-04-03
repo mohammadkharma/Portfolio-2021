af3b3199f5c2af63e69cfe8d560ed556e278376f
MYK <mohammad.kh.8688@gmail.com>
2021-04-03 19:21:34 +0200

    installed gatsby-image and configured the plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,20 @@ module.exports = {
   /* Your site config here */
   plugins: [
     "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects-md`,
         path: `${__dirname}/src/projects-md/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],

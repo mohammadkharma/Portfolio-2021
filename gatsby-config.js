dc47d628ee47e02c3d56321d8c58db0d05f447cf
MYK <mohammad.kh.8688@gmail.com>
2021-04-03 17:25:56 +0200

    working with gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `notes`,
+        path: `${__dirname}/src/notes/`,
+      },
+    },
+  ],
   siteMetadata: {
     title: "Mohammad Kharma",
     description: "Portfolio 2021",

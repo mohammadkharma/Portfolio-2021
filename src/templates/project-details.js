4529d2be2411ee4a46e948b0ddf5d6b740ca3a17
MYK <mohammad.kh.8688@gmail.com>
2021-04-04 12:48:19 +0200

    added styles a template for project-details generating

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,18 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
-import * as styles from "../styles/projects.module.css"
+import * as styles from "../styles/project-details.module.css"
 
 export default function ProjectDetails() {
   return (
     <div>
       <Layout>
-        <div className={styles.project - details}>
+        <div className={styles.projectDetails}>
           <h2>title</h2>
           <h3>stack</h3>
-          <div>
-            <Img fluid={} />
-          </div>
-          <div className={styles.html} dangerouslySetInnerHTML={} />
+          {/* <div><Img fluid={} /></div> */}
+          {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
         </div>
       </Layout>
     </div>

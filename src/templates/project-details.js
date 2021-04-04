78faf6333baf8951b15eb6710f446444c6339ad0
MYK <mohammad.kh.8688@gmail.com>
2021-04-04 13:14:00 +0200

    used query variable assigned in gatsby-node.js to fetch the data and generate a single project-details page for all projects

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,18 +1,47 @@
+import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/project-details.module.css"
+import Img from "gatsby-image"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
   return (
     <div>
       <Layout>
         <div className={styles.projectDetails}>
-          <h2>title</h2>
-          <h3>stack</h3>
-          {/* <div><Img fluid={} /></div> */}
-          {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+          <h2>{title}</h2>
+          <h3>{stack}</h3>
+          <div>
+            <Img fluid={featuredImg.childImageSharp.fluid} />
+          </div>
+          <div
+            className={styles.html}
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
         </div>
       </Layout>
     </div>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        stack
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`

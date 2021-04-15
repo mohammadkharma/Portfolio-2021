3baa9c317c0f3f729086232559f60d303a350ac5
MYK <mohammad.kh.8688@gmail.com>
2021-04-15 19:18:32 +0200

    added commented code

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,3 +37,36 @@ export const query = graphql`
     }
   }
 `
+
+// import React, { useContext } from "react"
+// import { css } from "@emotion/core"
+// import { useTheme } from "@emotion/react"
+// import Context from "../store/context"
+// import Layout from "../components/Layout"
+
+// const IndexPage = () => {
+//   const { state, dispatch } = useContext(Context)
+
+//   const theme = useTheme()
+
+//   return (
+//     <Layout>
+//       <div
+//         css={css`
+//           h1,
+//           h2 {
+//             color: ${state.isDark ? theme.dark.font : theme.light.font};
+//           }
+//         `}
+//       >
+//         <h1>Hello World</h1>
+//         <button onClick={() => dispatch({ type: "TOGGLE_DARK_MODE" })}>
+//           Toggle Dark Mode
+//         </button>
+//         <h2>Dark Mode is {state.isDark ? "on" : "off"}</h2>
+//       </div>
+//     </Layout>
+//   )
+// }
+
+// export default IndexPage

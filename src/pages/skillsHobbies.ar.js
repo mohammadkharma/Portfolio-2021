4f9ba366ba63d52d38bd10a4510978ecb3d16378
MYK <mohammad.kh.8688@gmail.com>
2021-04-29 23:38:41 +0200

    initial finish

--- src/pages/skillsHobbies.ar.js
+++ src/pages/skillsHobbies.ar.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Tilt from "react-vanilla-tilt"
 import Layout from "../components/Layout.ar"
 import * as styles from "../styles/about.module.css"
 
@@ -8,31 +9,35 @@ export default function SkillsHobbies() {
       <div className={styles.skillsHobbies}>
         <h2>المهارات والهوايات</h2>
         <div className={styles.skillsHobbiesContent}>
-          <section className={styles.skills}>
-            <p>
-              Iفي عالم التكنولوجيا الجديد ، من الجيد امتلاك بعض المهارات
-              الرقمية. لقد حسنت مهاراتي في <b>MS Office</b>, <b>Photoshop</b>,{" "}
-              <b>Video Edit</b> و <b>لغات وتقنيات البرمجة التالية</b>:
-            </p>
-            <ul>
-              <li>Javascript</li>
-              <li>CSS</li>
-              <li>HTML</li>
-              <li>React</li>
-              <li>Gastyb</li>
-            </ul>
-          </section>
-          <section className={styles.hobbies}>
-            <p>
-              بالنسبة للهوايات ، <b>كرة سلة</b> يأتي أولاً ، يليه:
-            </p>
-            <ul>
-              <li>قراءة</li>
-              <li>تعلم لغات أخرى</li>
-              <li>تنس طاولة</li>
-              <li>لعب الألعاب الرقمية</li>
-            </ul>
-          </section>
+          <Tilt className={styles.tilt}>
+            <section className={styles.skills}>
+              <p>
+                Iفي عالم التكنولوجيا الجديد ، من الجيد امتلاك بعض المهارات
+                الرقمية. لقد حسنت مهاراتي في <b>MS Office</b>, <b>Photoshop</b>,{" "}
+                <b>Video Edit</b> و <b>لغات وتقنيات البرمجة التالية</b>:
+              </p>
+              <ul>
+                <li>Javascript</li>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>React</li>
+                <li>Gastyb</li>
+              </ul>
+            </section>
+          </Tilt>
+          <Tilt className={styles.tilt}>
+            <section className={styles.hobbies}>
+              <p>
+                بالنسبة للهوايات ، <b>كرة سلة</b> يأتي أولاً ، يليه:
+              </p>
+              <ul>
+                <li>قراءة</li>
+                <li>تعلم لغات أخرى</li>
+                <li>تنس طاولة</li>
+                <li>لعب الألعاب الرقمية</li>
+              </ul>
+            </section>
+          </Tilt>
         </div>
       </div>
     </Layout>
--- src/pages/skillsHobbies.js
+++ src/pages/skillsHobbies.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Tilt from "react-vanilla-tilt"
 import Layout from "../components/Layout"
 import * as styles from "../styles/about.module.css"
 
@@ -8,32 +9,36 @@ export default function SkillsHobbies() {
       <div className={styles.skillsHobbies}>
         <h2>Skills & Hobbies</h2>
         <div className={styles.skillsHobbiesContent}>
-          <section className={styles.skills}>
-            <p>
-              In the new Tech world, it's good to have some digital skills. I
-              improved mine in <b>MS Office</b>, <b>Photoshop</b>,{" "}
-              <b>Video Edit</b> and
-              <b>the following programming languages and technologies</b>:
-            </p>
-            <ul>
-              <li>Javascript</li>
-              <li>CSS</li>
-              <li>HTML</li>
-              <li>React</li>
-              <li>Gastyb</li>
-            </ul>
-          </section>
-          <section className={styles.hobbies}>
-            <p>
-              As for Hobbies, <b>Basketball</b> comes first, followed by:
-            </p>
-            <ul>
-              <li>Reading</li>
-              <li>learning other languages</li>
-              <li>Table Tennis</li>
-              <li>Playing Digital Games</li>
-            </ul>
-          </section>
+          <Tilt className={styles.tilt}>
+            <section className={styles.skills}>
+              <p>
+                In the new Tech world, it's good to have some digital skills. I
+                improved mine in <b>MS Office</b>, <b>Photoshop</b>,{" "}
+                <b>Video Edit</b> and
+                <b>the following programming languages and technologies</b>:
+              </p>
+              <ul>
+                <li>Javascript</li>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>React</li>
+                <li>Gastyb</li>
+              </ul>
+            </section>
+          </Tilt>
+          <Tilt className={styles.tilt}>
+            <section className={styles.hobbies}>
+              <p>
+                As for Hobbies, <b>Basketball</b> comes first, followed by:
+              </p>
+              <ul>
+                <li>Reading</li>
+                <li>learning other languages</li>
+                <li>Table Tennis</li>
+                <li>Playing Digital Games</li>
+              </ul>
+            </section>
+          </Tilt>
         </div>
       </div>
     </Layout>

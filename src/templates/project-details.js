a07652cca061e4f395085ea12366e22821343d87
MYK <mohammad.kh.8688@gmail.com>
2021-04-29 12:27:58 +0200

    commented out

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,51 +1,51 @@
-import { graphql } from "gatsby"
-import React from "react"
-import Layout from "../components/Layout"
-import * as styles from "../styles/project-details.module.css"
-import Img from "gatsby-image"
+// import { graphql } from "gatsby"
+// import React from "react"
+// import Layout from "../components/Layout"
+// import * as styles from "../styles/project-details.module.css"
+// import Img from "gatsby-image"
 
-export default function ProjectDetails({ data }) {
-  const { html } = data.markdownRemark
-  const { title, stack, link, thumb } = data.markdownRemark.frontmatter
+// export default function ProjectDetails({ data }) {
+//   const { html } = data.markdownRemark
+//   const { title, stack, link, thumb } = data.markdownRemark.frontmatter
 
-  return (
-    <div>
-      <Layout>
-        <div className={styles.projectDetails}>
-          <h2>{title}</h2>
-          <h3>{stack}</h3>
+//   return (
+//     <div>
+//       <Layout>
+//         <div className={styles.projectDetails}>
+//           <h2>{title}</h2>
+//           <h3>{stack}</h3>
 
-          <div>
-            <Img className={styles.img} fluid={thumb.childImageSharp.fluid} />
-          </div>
-          <div className={styles.content}>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-            <p className={styles.link}>
-              click <a href={link}>here</a> to the visite the site
-            </p>
-          </div>
-        </div>
-      </Layout>
-    </div>
-  )
-}
+//           <div>
+//             <Img className={styles.img} fluid={thumb.childImageSharp.fluid} />
+//           </div>
+//           <div className={styles.content}>
+//             <div dangerouslySetInnerHTML={{ __html: html }} />
+//             <p className={styles.link}>
+//               click <a href={link}>here</a> to the visite the site
+//             </p>
+//           </div>
+//         </div>
+//       </Layout>
+//     </div>
+//   )
+// }
 
-export const query = graphql`
-  query ProjectDetails($slug: String) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        title
-        stack
-        link
-        thumb {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query ProjectDetails($slug: String) {
+//     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+//       html
+//       frontmatter {
+//         title
+//         stack
+//         link
+//         thumb {
+//           childImageSharp {
+//             fluid {
+//               ...GatsbyImageSharpFluid
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `

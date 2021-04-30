8ce69f82f57dba82e2dccd36c11d24d2acf6235c
MYK <mohammad.kh.8688@gmail.com>
2021-04-30 10:17:02 +0200

    debug

--- src/pages/projects/index.ar.js
+++ src/pages/projects/index.ar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Tilt from "react-vanilla-tilt"
 import { graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+// import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../../components/Layout.ar"
 import * as styles from "../../styles/projects.module.css"
 
@@ -17,11 +17,11 @@ export default function Projects({ data }) {
             <Tilt key={project.id} className={styles.tilt}>
               <div id="projects" className={styles.project}>
                 <div className={styles.content}>
-                  <GatsbyImage
+                  {/* <GatsbyImage
                     className={styles.img}
                     image={getImage(project.frontmatter.thumb)}
                     alt={project.frontmatter.slug}
-                  />
+                  /> */}
                   <h2>{project.frontmatter.title}</h2>
                   <h4>{project.frontmatter.stack}</h4>
                   <p className={styles.readMore}>⬇ اقرأ أكثر</p>
